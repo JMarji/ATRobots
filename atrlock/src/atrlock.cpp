ffdc6c057d7e8f60edb5f7245e08eee30abb24ef
The Doctor <sciencedude100@yahoo.com>
2018-01-23 14:56:06 -0500

    basic function and global variables setup

--- atrlock/src/atrlock.cpp
+++ atrlock/src/atrlock.cpp
@@ -2,6 +2,24 @@
 
 using namespace std;
 
+#define locktype 3
+
+string fn1, fn2, s, s1, s2, lock_code;
+char f1, f2;
+int i, j, k, lock_pos, lock_dat, this_dat;
+
 int main(int argc, char argv*){
   return EXIT_SUCCESS;
+}
+
+string encode(string s){
+  return "";
+}
+
+string prepare(string s, string s1){
+  return "";
+}
+
+var write_line(String s, String s1){
+  
 }
