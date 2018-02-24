43524c0514307502dd868a7591738c10a180788e
Unknown <marjij@sunyit.edu>
2018-02-24 16:46:24 -0500

    new typedef stringz
    struct stringz in termlib.cpp in order to port lines 184 and 186 , this is the best implemntation i could think of

--- robotic-codes/src/termlib.cpp
+++ robotic-codes/src/termlib.cpp
@@ -7,15 +7,18 @@ using namespace std;
 namespace termlib
 {
 
+  typedef struct
+  {
+    string[max_var_len];
+  } stringz; // struct made in order to make array of array of strings varname
+
 int num_robots, numvars, numlabels, maxcode, lock_pos, lock_dat, game_delay,
 time_slice, insanity, update_timer, max_gx, max_gy, stats_mode, kill_count,
 report_type;
 
 int varloc[(max_vars + 1)], labelnum[(max_labels + 1)];
-struct  { // array of array of string make it happen
-  /* data */
-};ing var[max_var_len];
 
+stringz varname[(max_vars + 1)], labelname[(max_vars + 1)];
 
 //flag for viewing debugger panel vs. robot stats
 bool show_source, compile_only, old_shields, insane_missiles, debug_info, windoze,
