6a3b631b6d37524af0bd1bd2027f0670a3c6aaea
Unknown <juddlestix@gmail.com>
2018-03-26 20:16:17 -0400

    Created op_rec and prog_type struct

--- robotic-codes/src/termlib.cpp
+++ robotic-codes/src/termlib.cpp
@@ -2,8 +2,19 @@
 #include <cstdlib>
 #include "termlib.hpp"
 
+#define max_op 3
+#define max_code 1023
+
 using namespace std;
 
 namespace termlib{
-  
+  typedef struct
+  {
+    int op[max_op];
+  }op_rec;
+
+  typedef struct
+  {
+    op_rec oprec[max_code];
+  }prog_type;
 }
