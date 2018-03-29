c64c061c38b9f2a2a1be93e565245c9b4d2cc732
Unknown <juddlestix@gmail.com>
2018-03-29 15:31:16 -0400

    More test things

--- robotic-codes/src/robtest.cpp
+++ robotic-codes/src/robtest.cpp
@@ -12,7 +12,9 @@ int main (void)
   mine_rec testMine = robot.getMine(0);
   testMine.x = 300;
   cout << "Mine x: "<< testMine.x << endl;
-
-
+  for (int i = 0; i <= 63; i++)
+  {
+    robot.addMine();
+  }
   return 0;
 }
