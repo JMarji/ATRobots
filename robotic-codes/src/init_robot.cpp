f95cc6231485446a3c89dc60b255f3d8aac09195
Unknown <juddlestix@gmail.com>
2018-04-03 15:07:29 -0400

    Things to allow init_robot to work

--- robotic-codes/src/init_robot.cpp
+++ robotic-codes/src/init_robot.cpp
@@ -1,29 +1,42 @@
+#include "robclass.cpp"
+
+using namespace std;
+
+robot_rec robot[16];
+
+int main(void)
+{
+  for (int i = 0; i <= 16; i++)
+  {
+    init_robot(i);
+  }
+}
 void init_robot (int n)
 {
   int i, j, k, l;
 
-  robot[i].setWins(0);
-  robot[i].setTrials(0);
-  robot[i].setKills(0);
-  robot[i].setDeaths(0);
-  robot[i].setShots_fired(0);
-  robot[i].setMatch_shots(0);
-  robot[i].setHits(0);
-  robot[i].setDamage_total(0);
-  robot[i].setCycles_lived(0);
-  robot[i].setError_count(0);
-  robot[i].setPlen(0);
-  robot[i].setMax_time(0);
-  robot[i].setName(" ");
-  robot[i].setFn(" ");
-  robot[i].setSpeed(0);
-  robot[i].setArc_count(0);
-  robot[i].setSonar_count(0);
-  robot[i].setRobot_time_limit(0);
-  robot[i].setScanRange(1500);
-  robot[i].setShotStrength(1);
-  robot[i].setDamageAdj(1);
-  robot[i].setSpeedAdj(1);
-  robot[i].setMines(0);
-  robot[i].setConfig_scanner(5);
+  robot[n].setWins(0);
+  robot[n].setTrials(0);
+  robot[n].setKills(0);
+  robot[n].setDeaths(0);
+  robot[n].setShots_fired(0);
+  robot[n].setMatch_shots(0);
+  robot[n].setHits(0);
+  robot[n].setDamage_total(0);
+  robot[n].setCycles_lived(0);
+  robot[n].setError_count(0);
+  robot[n].setPlen(0);
+  robot[n].setMax_time(0);
+  robot[n].setName(" ");
+  robot[n].setFn(" ");
+  robot[n].setSpeed(0);
+  robot[n].setArc_count(0);
+  robot[n].setSonar_count(0);
+  robot[n].setRobot_time_limit(0);
+  robot[n].setScanRange(1500);
+  robot[n].setShotStrength(1);
+  robot[n].setDamageAdj(1);
+  robot[n].setSpeedAdj(1);
+  robot[n].setMines(0);
+  robot[n].setConfig_scanner(5);
 }
