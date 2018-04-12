c49ec70c6357fae0aa76244602fcc5db34a97c03
joe_marji <marjij@sunyit.edu>
2018-04-12 15:09:11 -0400

    updates setters

--- robotic-codes/src/robclass.cpp
+++ robotic-codes/src/robclass.cpp
@@ -1,4 +1,58 @@
 
+/*rough skelton for the PAS code starting at line 131 in ATR2.PAS the type
+/statement
+*/
+
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <vector>
+#include "termlib.hpp"
+
+#define max_robot_lines 8
+#define max_ram 1023
+#define max_mines 63
+
+using namespace std;
+
+typedef struct
+{
+  double x, y;
+  int detect, yield;
+  bool detonate;
+}mine_rec;
+
+typedef struct
+{
+  double x, y, lx, ly, mult, mspd;
+  int source, a, hd, rad, lrad, max_rad;
+}missle_rec;
+
+class robot_rec
+{
+  private:
+    bool is_locked,shields_up,lshields,overburn,keepshift,cooling,won;
+    int mem_watch,hd,thd,lhd,spd,tspd,armor,larmor,heat,lheat,ip,
+     plen,scanarc,accuracy,shift,err,delay_left,robot_time_limit,
+     max_time,time_left,lshift,arc_count,sonar_count,scanrange,
+     last_damage,last_hit,transponder,shutdown,channel,
+     lendarc,endarc,lstartarc,startarc,mines;
+
+    long wins,trials,kills,deaths,startkills,shots_fired,match_shots,
+    hits,damage_total,cycles_lived,error_count;
+
+    int tx[(max_robot_lines - 1)], ltx[(max_robot_lines - 1)],
+     ty[(max_robot_lines - 1)], lty[(max_robot_lines - 1)], ram[max_ram + 1]; //max_ram might be declared as max rad by mistake !!!
+
+    double x,y,lx,ly,xv,yv,speed,shotstrength,damageadj,speedadj,meters;
+
+    config_rec config; // robclass::config_rec ?? // line 158 in ATR2.PAS not sure how this would
+    //good question to ask Dr.Confer
+    // code = new prog_type;
+    //line 159 in ATR2.PAS should we make a char array or a string type?
+    string name, fn;
+    vector<mine_rec> mine;
+   //errorlog how should we implemnt the text data type
 
   public:
     prog_type code;
@@ -132,7 +186,7 @@
     {
       transponder = x;
     }
-    
+
     void set_wins(long x)
     {
       wins = x;
